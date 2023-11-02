26a8d43f871633eee32a7dd5b4a5620f86f13c91
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2023-11-03 00:52:27 +0800

    add class.rs

---
+++ rust/src/bin/class.rs
@@ -0,0 +1,19 @@
+struct Person {
+    name: String,
+    age: i32,
+}
+
+impl Person {
+    fn new(name: String, age: i32) -> Self {
+        Self { name, age }
+    }
+
+    fn print_info(&self) {
+        println!("I'm {}, {} year old", self.name, self.age)
+    }
+}
+
+fn main() {
+    let p = Person::new(String::from("Ahri"), 13);
+    p.print_info();
+}
